19fd58faa21c902dc9c680fa2d021e15a64b422e
Matthew Sallin <msallin@gmail.com>
2020-09-24 22:06:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(name_hash)
-  if name_hash == nil 
+  if name_hash.empty? 
     nil
   else
   lowestValue = 1000

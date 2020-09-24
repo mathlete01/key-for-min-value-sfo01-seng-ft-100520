48eb3be89fe70fa8f508f10be8949555f2212596
Matthew Sallin <msallin@gmail.com>
2020-09-24 22:07:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ def key_for_min_value(name_hash)
   if name_hash.empty? 
     nil
   else
-    lowestValue = 10
+    lowestValue = 100
     lowestKey = ""
     name_hash.each do |key, value|
       if value < lowestValue 

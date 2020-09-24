404ba389d9eecd7e06fd01af786258d27280eacf
Matthew Sallin <msallin@gmail.com>
2020-09-24 21:53:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(name_hash)
   #puts "{name_hash} = #{name_hash}"
-  box = 1000
+  b
+  ox = 1000
   name_hash.each do |key, value|
     #puts "#{key}: #{value}"
     if value < box 
       box = value
       puts "box = #{box}"
+      puts "key = #{key}"
     end
   end
 end
